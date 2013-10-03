2ac5291046eed792f57b1db35c875a6c220acd5b
Marek Mościchowski <moscich@gmail.com>
2013-10-03 18:07:36 +0200

    Initial Commit

---
+++ LoremPixum/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LoremPixum
+//
+//  Created by Marek Mościchowski on 03.10.2013.
+//  Copyright (c) 2013 Marek Mościchowski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LoremPixumTests/LoremPixumTests.m
@@ -0,0 +1,34 @@
+//
+//  LoremPixumTests.m
+//  LoremPixumTests
+//
+//  Created by Marek Mościchowski on 03.10.2013.
+//  Copyright (c) 2013 Marek Mościchowski. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LoremPixumTests : XCTestCase
+
+@end
+
+@implementation LoremPixumTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
